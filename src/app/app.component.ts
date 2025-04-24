82b1d5a9a9925ee9ba89235832a50b5185680482
Billal Hossain (Tousif) <tuhin.world@gmail.com>
2025-04-24 18:35:45 +0600

    Add app configs and environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {DeadlineCounterComponent} from './deadline-counter/deadline-counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DeadlineCounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://apiv2.getkexy.com/v2' // Your backend URL
+};
